96af733b51ecee2717237214e65dcbe4790421ac
JohnnyHdez <90969013+JohnnyHdez@users.noreply.github.com>
2024-05-02 21:54:30 -0500

    Delete anime_list directory

--- anime_list/arbol_xml.py
+++
@@ -1,8 +0,0 @@
-import xml.etree.ElementTree as et
-import os
-
-file_path = os.path.join("ddbb", "animes.xml")
-root = et.parse(source=file_path)
-main_element = root.getroot()
-
-print(main_element)
